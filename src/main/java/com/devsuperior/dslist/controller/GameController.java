e2c51d8bb3cb9e1aaa4aefb1f9e3b9639cc4f436
Miguelito2926 <juniorprol@hotmail.com>
2023-07-12 15:13:06 -0300

    GET game by id

--- src/main/java/com/devsuperior/dslist/controller/GameController.java
+++ src/main/java/com/devsuperior/dslist/controller/GameController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.services.GameService;
 
@@ -19,6 +21,12 @@ public class GameController {
 	
 	@Autowired // Injeta o gameService
 	private GameService gameService;
+	
+	@GetMapping(value = "/{id}") // configura o mapeamento passando o id como parametro
+	public  GameDTO findById(@PathVariable Long id) { // @PathVariable indentifica o paramentro passado na url do recurso 
+		GameDTO resultado = gameService.findById(id);
+		return resultado;
+	}
 	
 	@GetMapping //mapea com verbo http get
 	public List<GameMinDTO> findAll(){ // metodo retorna uma lista de game service
--- src/main/java/com/devsuperior/dslist/services/GameService.java
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -4,10 +4,14 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
 import com.devsuperior.dslist.repositories.GameRepository;
+
+
 
 // Classe respponsavel pelas regras de negocios e também que executa os metodos consulta, exclusão, adição e atualização ao banco.
 
@@ -17,7 +21,18 @@ public class GameService {
 	@Autowired // injeta uma instancia de repositorio
 	private GameRepository gameRepository;
 	
+	//Retorna um consulta por id
+	@Transactional(readOnly = true)
+	public GameDTO findById(Long id) {
+		Game resultado = gameRepository.findById(id).get();
+		GameDTO dto = new GameDTO(resultado);
+		return dto;
+		
+	}
+	
+	
 	// Metodo para retonar uma lista de todos que esxiste em GameMinDTO 
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll(){
 		List<Game> resultado = gameRepository.findAll();
 		
